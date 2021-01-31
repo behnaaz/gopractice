d7ba48f1141f66967aaf346f059b3800fabe41c2
Behnaz <behnaz.changizi@gmail.com>
2021-01-31 20:00:54 +0000

    Add class for mainlogic

--- first/src/prj/medable.go
+++ first/src/prj/medable.go
@@ -7,6 +7,36 @@ type Medable struct {
 	IsFirst bool
 }
 
+type Median struct {
+	Val  float32
+	Less int
+}
+
 func NewMedable(Ar []int, First bool) Medable {
 	return Medable{Nums: Ar, IsFirst: First, Beg: 0, End: len(Ar) - 1}
+}
+
+func midex(m Medable) int {
+	return (m.Beg + m.End) / 2
+}
+
+func ShiftBeg(m Medable) {
+	m.Beg = midex(m) - 1
+}
+
+func ShiftEnd(m Medable) {
+	m.End = midex(m) + 1
+}
+
+func IsDone(m Medable) bool {
+	return (m.Beg > m.End)
+}
+
+func CalcMedian(m Medable) Median {
+	a := m.Nums
+	v := float32(a[midex(m)])
+	if len(a)%2 == 0 {
+		v = float32((a[midex(m)] + a[midex(m)+1]) / 2)
+	}
+	return Median{Val: v, Less: midex(m)}
 }
---
+++ first/src/prj/medianoftwo.go
@@ -0,0 +1,7 @@
+package main
+
+func CalcMedianOfTwo(m1 Medable, m2 Medable) float32 {
+	a := m1.Nums
+	b := m2.Nums
+	return 0
+}
