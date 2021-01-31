969de8d08fb89cea429ca74b11b1b7aff9caf77a
Behnaz <behnaz.changizi@gmail.com>
2021-01-31 19:26:57 +0000

    bug fix

--- first/src/prj/medable.go
+++ first/src/prj/medable.go
@@ -12,17 +12,19 @@ func NewMedable(Ar []int, First bool) Medable {
 }
 
 func Lessers(M Medable, F float32) int {
-	return lessers(M, F, 0, len(M.Nums))
+	return lessers(M, F, 0, len(M.Nums)-1)
 }
 
 func lessers(M Medable, F float32, B int, E int) int {
-	if B < 0 {
+	if B < 0 || E < 0 {
 		return 0
 	}
+
 	a := M.Nums
 	if E >= len(a) {
 		return len(a)
 	}
+
 	if B == E {
 		t := float32(a[B])
 		if F < t || (M.IsFirst && F == t) {
@@ -30,11 +32,13 @@ func lessers(M Medable, F float32, B int, E int) int {
 		}
 		return B + 1
 	}
+
 	idx := (B + E) / 2
 	t := float32(a[idx])
 	if t > F {
 		return lessers(M, F, B, idx-1)
 	}
+
 	if t < F && (idx == len(a)-1 || (float32(a[idx+1]) == F && !M.IsFirst) || float32(a[idx+1]) > F) {
 		return idx
 	}
--- first/src/prj/medmain.go
+++ first/src/prj/medmain.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	m := []int{2, 3, 5, 7, 11, 13}
 	a := NewMedable(m, true)
-	l := Lessers(a, 3.3)
-	fmt.Print("hi %d", l)
+	l := Lessers(a, 2)
+	fmt.Print(a)
+	fmt.Print(l)
 }
